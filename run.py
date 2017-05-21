7ded2e935a4dde31272ac8c5e7151349b6904e20
Jack Grahl <jack.grahl@gmail.com>
2017-05-21 08:33:46 +0000

    Add autodoc.

--- run.py
+++ run.py
@@ -1,11 +1,14 @@
 from flask import Flask
 
+from extensions import autodoc
+
 from tools import tools_blueprint
 
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(tools_blueprint)
+    autodoc.init_app(app)
     return app
 
 
--- tools.py
+++ tools.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from extensions import autodoc
 
 tools_blueprint = Blueprint(
     'tools',
@@ -10,3 +11,8 @@ tools_blueprint = Blueprint(
 def health_check():
     """ Check that the app is up and running. """
     return "OK", 200
+
+
+@tools_blueprint.route('/docs')
+def html_doc():
+    return autodoc.html()
